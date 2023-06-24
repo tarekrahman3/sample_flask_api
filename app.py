b7d6495781a05b8ef5c888047b7bdd85b4130ab1
Tarek Rahman <44648395+tarekrahman3@users.noreply.github.com>
2023-06-24 01:33:02 +0000

    g

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+from flask import render_template
+
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+DOCUMENT_ID = '1TmjDLbyQjlQoFDhmNIkieH0wJY16eG4BtOcZsi4gq_8'
+
+
+def t(f):
+    credentials = service_account.Credentials.from_service_account_file(
+        f,
+        scopes=[
+            "https://www.googleapis.com/auth/calendar",
+            "https://www.googleapis.com/auth/calendar.events",
+            "https://www.googleapis.com/auth/spreadsheets.currentonly",
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    sheet_service = googleapiclient.discovery.build("sheets", "v4", credentials=credentials)
+    r=sheet_service.spreadsheets().get(spreadsheetId=DOCUMENT_ID)
+    response = r.execute()
+    print(response)
+    return response
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Success!'
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
 
-@app.route('/api', methods=['GET'])
-def get_data():
-    data = {
-        'id': 00,
-        'name': 'Tarek Rahman',
-        'email': 'tarek@example.com'
-    }
-    return jsonify(data)
+@app.route('/', methods=['POST'])
+def handle_upload():
+    print('file ..')
+    file = request.files['file']
+    file.save('./s.json')
+    print(dir(file))
+    
+    return {'message':  t('./s.json')}
 
 if __name__ == '__main__':
     app.run()
